c7954958964c1248f884d4e80ceebae271e56f0c
fenil2144 <shahfenil1998@gmail.com>
2023-04-11 23:46:59 +0530

    10 Apr 2023

--- 05-10Apr2023-MavenDemoSpringInitializr/src/main/java/com/example/MavenDemoSpringInitializr/controller/BookController.java
+++ 05-10Apr2023-MavenDemoSpringInitializr/src/main/java/com/example/MavenDemoSpringInitializr/controller/BookController.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.MavenDemoSpringInitializr.model.Book;
@@ -43,6 +47,27 @@ public class BookController {
 		//Book@472698d  - Singleton Instance Created By Spring
 		//Book@472698d
 		//Book@472698d
+	}
+	
+	//getBook - GET - Param - Title
+	//updateBook - Put - Path Variable - bookId
+	//saveBook - Post - Request Body
+	
+	@GetMapping("/requestParam")
+	public void getDataFromParam(@RequestParam(required = false,defaultValue = "Java") String title,
+			@RequestParam int price) {
+		logger.info(title+" and "+price);
+	}
+	
+	@GetMapping("/pathVariable/{bookId}/{ISBNNumber}")
+	public void getDatafromPathVariable(@PathVariable("bookId") int bookId, @PathVariable("ISBNNumber") String IsbnNo) {
+		logger.info(bookId + " - "+IsbnNo);
+	}
+	
+	@PostMapping("/requestBody")
+	public void getDataFromRequestBody(@RequestBody Book book) {
+		logger.info(book.toString());
+		
 	}
 
 }
--- 05-10Apr2023-MavenDemoSpringInitializr/src/main/java/com/example/MavenDemoSpringInitializr/model/Book.java
+++ 05-10Apr2023-MavenDemoSpringInitializr/src/main/java/com/example/MavenDemoSpringInitializr/model/Book.java
@@ -4,16 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 	
 	private static Logger logger = LoggerFactory.getLogger(Book.class);
 	
 	private int id;
 	private String name;
-	
-	public Book() {
-		logger.info("Book Object in Book Class {}",this);
-	}
 
 }
